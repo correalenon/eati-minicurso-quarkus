cd56569113f144759fa54d774be490907d2fe462
Lenon Corrêa <lenonac_@hotmail.com>
2023-11-15 11:04:06 -0300

    Adiciona JogadorDTO

---
+++ jogadores/src/main/java/info/eati/quarkus/dto/JogadorDTO.java
@@ -0,0 +1,12 @@
+package info.eati.quarkus.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JogadorDTO {
+    private String nome;
+    private String time;
+    private int camisa;
+}
