0052d74c7d7787e879534de3e6d1d7ec865b6db8
Lenon Corrêa <lenonac_@hotmail.com>
2023-11-15 11:05:29 -0300

    Adiciona inserirJogador e deletarJogadorPeloId

--- jogadores/src/main/java/info/eati/quarkus/resource/JogadorResource.java
+++ jogadores/src/main/java/info/eati/quarkus/resource/JogadorResource.java
@@ -2,10 +2,13 @@ package info.eati.quarkus.resource;
 
 import java.util.List;
 
+import info.eati.quarkus.dto.JogadorDTO;
 import info.eati.quarkus.entity.Jogador;
 import info.eati.quarkus.service.JogadorService;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 
@@ -27,5 +30,22 @@ public class JogadorResource {
 	public Response consultarJogadorPeloId(Long id) {
 		Jogador jogador = jogadorService.consultarPeloId(id);
 		return Response.ok().entity(jogador).build();
+	}
+
+	@Path("/{id}")
+	@DELETE
+	public Response deletarJogadorPeloId(Long id) {
+		boolean jogador = jogadorService.deletarPeloId(id);
+		if (jogador) {
+			return Response.noContent().build();
+		}
+		return Response.notModified().build();
+	}
+
+	@Path("/criar")
+	@POST
+	public Response inserirJogador(JogadorDTO jogadorDTO) {
+		Jogador jogador = jogadorService.inserir(jogadorDTO);
+		return Response.ok(jogador).status(201).build();
 	}
 }
